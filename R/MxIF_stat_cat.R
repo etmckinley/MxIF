9f10d5d77a90dc48456dde72f3cbb05215416099
Eliot McKinley <etmckinley@gmail.com>
2017-10-04 16:18:17 -0500

    added stat concatenation

---
+++ R/MxIF_stat_cat.R
@@ -0,0 +1,20 @@
+#' MxIF Stat Cat
+#'
+#' This function concatenates stats files into one CSV.
+#' Eliot McKinley 9/15/17
+#' @param file location of folder containing stats files
+#' @keywords stats concatenate MxIF
+#' @export
+#' @examples
+#' MxIF.stat.cat(file)
+
+MxIF.stat.cat <- function(file){
+  stats.files=list.files(path = file, pattern="\\.csv$", full.names = TRUE)
+  alldata=NULL
+  for (i in 1:length(stats.files)){
+    dat0=read.csv(file=stats.files[i], header=TRUE)
+    alldata=rbind(alldata,dat0)
+  }
+  
+  return(alldata)  
+}
