ff5d4d6140e6497b4ce0c80e3eb3be0ed8ee098f
Eliot McKinley <etmckinley@gmail.com>
2017-10-05 12:25:25 -0500

    Allows renaming tSNE coordinates

--- R/MxIF_tSNE.R
+++ R/MxIF_tSNE.R
@@ -4,6 +4,7 @@
 #' Eliot McKinley 9/15/17
 #' @param data data frame of cell data from Matlab based segmentation
 #' @param markers columns for each marker to run tSNE on
+#' @param label column name for tSNE, default= c('tSNE1', 'tSNE2')
 #' @param seed set seed for tSNE, default=42
 #' @param perplexity, set perplexity for tSNE, default=50
 #' @param iterations set iterations for tSNE , default=1000
@@ -12,18 +13,21 @@
 #' @export
 #' @examples
 #' MxIF.tsne(data, c(1,2, 5:9))
-#' MxIF.tsne(data, c(1:5, 6 ,8) seed=50, perplexity=30, iterations= 500, verbose=FALSE)
+#' MxIF.tsne(data, c(1:5, 6 ,8), label=('alltSNE1','alltSNE2') seed=50, perplexity=30, iterations= 500, verbose=FALSE)
 #' 
 
-MxIF.tsne <- function(data, markers, seed=42 , perplexity=50, iterations=1000, verbose=TRUE){
+MxIF.tsne <- function(data, markers, label=c('tSNE1', 'tSNE2'), seed=42 , perplexity=50, iterations=1000, verbose=TRUE){
+  library(Rtsne)
   
   set.seed(seed)
   rtsne_out <- Rtsne(as.matrix(data[,markers]), verbose=verbose, perplexity=perplexity, max_iter=iterations, dims=2)
   
   #insert tSNE coordinates into table
-  data$tSNE1= rtsne_out$Y[,1] 
-  data$tSNE2= rtsne_out$Y[,2]
+  data$rtSNE1= rtsne_out$Y[,1] 
+  data$rtSNE2= rtsne_out$Y[,2]
   
+  names(data)[which(names(data)=="rtSNE1")]=label[1]
+  names(data)[which(names(data)=="rtSNE2")]=label[2]
   return(data)
   
 }
